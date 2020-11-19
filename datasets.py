7486d02ae16eb82abc6568b6713118072e9ffb35
Direnc <direnc99@gmail.com>
2020-11-19 19:20:03 +0100

    general exploration, white wine model

--- datasets.py
+++ datasets.py
@@ -3,8 +3,8 @@ import pandas as pd
 wine_quality_red1 = pd.read_csv('./data/winequality-red-1.csv', sep=";", decimal=',')
 wine_quality_red2 = pd.read_csv('./data/winequality-red-2.csv', sep=";", decimal=',')
 
-wine_quality_white1 = pd.read_csv('./data/winequality-white-1.csv', sep=";")
-wine_quality_white2 = pd.read_csv('./data/winequality-white-2.csv', sep=";")
+wine_quality_white1 = pd.read_csv('./data/winequality-white-1.csv', sep=";", decimal=',')
+wine_quality_white2 = pd.read_csv('./data/winequality-white-2.csv', sep=";", decimal=',')
 
 wine_red = pd.merge(left=wine_quality_red1, right= wine_quality_red2, on='ID')
 wine_white = pd.merge(left=wine_quality_white1, right= wine_quality_white2, on='ID')
