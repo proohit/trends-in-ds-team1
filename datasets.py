646efb753b894806edb096c14d1c8541606cb488
proohit <direnc99@gmail.com>
2020-11-25 20:27:14 +0100

    beginning data quality issues
    - missing values

--- datasets.py
+++ datasets.py
@@ -1,16 +1,24 @@
 import pandas as pd
 
-wine_quality_red1 = pd.read_csv('./data/winequality-red-1.csv', sep=";", decimal=',')
-wine_quality_red2 = pd.read_csv('./data/winequality-red-2.csv', sep=";", decimal=',')
+wine_quality_red1 = pd.read_csv(
+    './data/winequality-red-1.csv', sep=";", decimal=',')
+wine_quality_red2 = pd.read_csv(
+    './data/winequality-red-2.csv', sep=";", decimal=',')
 
-wine_quality_white1 = pd.read_csv('./data/winequality-white-1.csv', sep=";", decimal=',')
-wine_quality_white2 = pd.read_csv('./data/winequality-white-2.csv', sep=";", decimal=',')
+wine_quality_white1 = pd.read_csv(
+    './data/winequality-white-1.csv', sep=";", decimal=',')
+wine_quality_white2 = pd.read_csv(
+    './data/winequality-white-2.csv', sep=";", decimal=',')
 
-wine_red = pd.merge(left=wine_quality_red1, right= wine_quality_red2, on='ID')
-wine_white = pd.merge(left=wine_quality_white1, right= wine_quality_white2, on='ID')
+wine_red = pd.merge(left=wine_quality_red1,
+                    right=wine_quality_red2, on='ID', how='outer')
+wine_white = pd.merge(left=wine_quality_white1,
+                      right=wine_quality_white2, on='ID')
+
 
 def wine_red_dataset():
-    return pd.merge(left=wine_quality_red1, right= wine_quality_red2, on='ID')
+    return pd.merge(left=wine_quality_red1, right=wine_quality_red2, on='ID')
+
 
 def wine_white_dataset():
-    return pd.merge(left=wine_quality_white1, right= wine_quality_white2, on='ID')
+    return pd.merge(left=wine_quality_white1, right=wine_quality_white2, on='ID')
