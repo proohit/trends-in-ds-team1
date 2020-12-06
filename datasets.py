37b827e2ebe52464aa9d5ae305695b0b5a1b518a
proohit <direnc99@gmail.com>
2020-12-06 21:01:07 +0100

    cleanup general exploration

--- datasets.py
+++ datasets.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
@@ -30,6 +32,16 @@ def wine_white_dataset():
 def get_wine_red_features():
     wine_red = wine_red_dataset()
     return wine_red.drop('ID', axis=1).drop('quality', axis=1)
+
+
+def get_wine_white_label():
+    wine_white = wine_white_dataset()
+    return wine_white['quality']
+
+
+def get_wine_white_features():
+    wine_white = wine_white_dataset()
+    return wine_white.drop('ID', axis=1).drop('quality', axis=1)
 
 
 def get_wine_red_label():
@@ -72,3 +84,19 @@ def standardize_feature(data, feature):
     new_data = data.copy()
     new_data[feature] = feature_normalized
     return new_data
+
+
+def get_delta_mean_median(data):
+    median = data.median()
+    mean = data.mean()
+    delta_mean_median = (median - mean).round(5)
+    quotient = (delta_mean_median / mean) * 100
+    quotient = np.sqrt(quotient ** 2)
+    return (quotient, median)
+
+
+def show_scatterplot(data, x, y, x_label='feature', y_label='label'):
+    plt.scatter(y=data[y], x=data[x])
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.show()
